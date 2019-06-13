cae3595a8764c9a9d7db00fcf52a5b21db79cb5c
Henry Cheung <henrycheung@joyaether.com>
2019-06-13 14:43:06 +0800

    updated mobile menu text and hyperlink

--- src/components/100Include/mobileMenu.js
+++ src/components/100Include/mobileMenu.js
@@ -2,31 +2,32 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { withTranslation } from 'react-i18next';
 
-import {
-	Accordion,
-	AccordionItem,
-	AccordionItemTitle,
-	AccordionItemBody,
-} from 'react-accessible-accordion';
+// import {
+// 	Accordion,
+// 	AccordionItem,
+// 	AccordionItemTitle,
+// 	AccordionItemBody,
+// } from 'react-accessible-accordion';
 
 import 'react-accessible-accordion/dist/minimal-example.css';
 
-import * as HelperMobileHandle from '../../utils/00JqueryControl/MobileHandle';
+// import * as HelperMobileHandle from '../../utils/00JqueryControl/MobileHandle';
 
 // import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
 // import * as AnimationsActionCreators from '../../actions/animations';
 
 function MobileMenu(props) {
-	const { t, i18n } = props;
+	const { //t, 
+		i18n } = props;
 
-	let handleMobileMenuReset = () => {
-		HelperMobileHandle.MobileHandle.menuReset();
-	}
+	// let handleMobileMenuReset = () => {
+	// 	HelperMobileHandle.MobileHandle.menuReset();
+	// }
 
-	let logined = false;
-	if (typeof (props.auth.auth) !== "undefined" && props.auth.auth !== null && props.auth.auth.length !== 0)
-		logined = true;
+	// let logined = false;
+	// if (typeof (props.auth.auth) !== "undefined" && props.auth.auth !== null && props.auth.auth.length !== 0)
+	// 	logined = true;
 
 	return (
 		<div className="wrapper-mobile-menu">
@@ -39,47 +40,14 @@ function MobileMenu(props) {
 				<div className="sep-20"></div>
 
 				<ul>
-					<li>
-						<Accordion>
-							<AccordionItem>
-								<AccordionItemTitle>
-									<h3>{t("General.ForParkers")}</h3>
-								</AccordionItemTitle>
-								<AccordionItemBody>
-									<Link to={"/" + i18n.language + "/"}
-										onClick={handleMobileMenuReset}
-									>{t("Menu.HowItWork")}</Link>
-								</AccordionItemBody>
-							</AccordionItem>
-							<AccordionItem>
-								<AccordionItemTitle>
-									<h3>{t("General.ForOwners")}</h3>
-								</AccordionItemTitle>
-								<AccordionItemBody>
-									<Link to={"/" + i18n.language + "/"}
-										onClick={handleMobileMenuReset}
-									>{t("Menu.HowItWork")}</Link>
-								</AccordionItemBody>
-							</AccordionItem>
-						</Accordion>
-					</li>
-
-					{logined &&
-						<li><Link to={"/" + i18n.language + "/"}>{t("General.MyAccount")}</Link></li>
-					}
-					{logined &&
-						<li><Link to={"/" + i18n.language + "/memberLogout"}>{t("General.Logout")}</Link></li>
-					}
-					{!logined &&
-						<li><Link to={"/" + i18n.language + "/memberCreateAccountStart"}>{t("General.Register")}</Link></li>
-					}
-					{!logined &&
-						<li><Link to={"/" + i18n.language + "/memberGetStarted"}>{t("General.Login")}</Link></li>
-					}
-					{i18n.language === 'zh-HK' ?
-						<li><Link to={"/en-US" + props.router.location.pathname.substring(6)} onClick={() => i18n.changeLanguage('en-US')}>Eng</Link></li>
-						: <li><Link to={"/zh-HK" + props.router.location.pathname.substring(6)} onClick={() => i18n.changeLanguage('zh-HK')}>繁</Link></li>
-					}
+					<li><Link to={"/" + i18n.language + "/courses-management"}>课程管理</Link></li>
+					<li><Link to={"/" + i18n.language + "/students-management"} className="active">学生管理</Link></li>
+					<li><Link to={"/" + i18n.language + "/enrollment-management"}>报名管理</Link></li>
+					<li><Link to={"/" + i18n.language + "/news-management"}>新闻管理</Link></li>
+					<li><Link to={"/" + i18n.language + "/activities-management"}>活动管理</Link></li>
+					<li><Link to={"/" + i18n.language + "/resources-management"}>资源管理</Link></li>
+					<li><Link to={"/" + i18n.language + "/my-alerts"}>我的提醒 (3)</Link></li>
+					<li><Link to={"/" + i18n.language + "/reports"}>项目报告</Link></li>
 				</ul>
 
 				<div className="sep-30"></div>
