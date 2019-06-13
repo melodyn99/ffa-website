1592e410d91b532cffb75e1740242974d7efb6e7
Henry Cheung <henrycheung@joyaether.com>
2019-06-13 16:29:10 +0800

    test

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-// import {Redirect} from 'react-router';
+// import {Redirect} from 'react-router'; hello
 import './css/App.scss';
 import { withTranslation } from 'react-i18next';
 
