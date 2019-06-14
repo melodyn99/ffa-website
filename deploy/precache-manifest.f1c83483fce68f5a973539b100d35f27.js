5e99d01cb96588710167f44b151022b23fcc0195
Henry Cheung <henrycheung@joyaether.com>
2019-06-14 18:34:26 +0800

    testing automation

---
+++ deploy/precache-manifest.f1c83483fce68f5a973539b100d35f27.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "ae228868ec62dd908f52eacf40571923",
+    "url": "/static/media/mp3.ae228868.png"
+  },
+  {
+    "revision": "ed67de23c432f532fd0c",
+    "url": "/static/css/main.09ce59e6.chunk.css"
+  },
+  {
+    "revision": "44b2929d1743ab37e0113002358c72db",
+    "url": "/static/media/unknown.44b2929d.png"
+  },
+  {
+    "revision": "42437928e274666857b1",
+    "url": "/static/js/1.42437928.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "ed67de23c432f532fd0c",
+    "url": "/static/js/main.ed67de23.chunk.js"
+  },
+  {
+    "revision": "25860965479de7e9c42c02a8b9775e90",
+    "url": "/static/media/ppt.25860965.png"
+  },
+  {
+    "revision": "7be1227cb65669d1333b7231916637d4",
+    "url": "/static/media/xls.7be1227c.png"
+  },
+  {
+    "revision": "ffe15f591bb7c8345e4db92abcd8f9dc",
+    "url": "/static/media/doc.ffe15f59.png"
+  },
+  {
+    "revision": "42437928e274666857b1",
+    "url": "/static/css/1.d00c6e20.chunk.css"
+  },
+  {
+    "revision": "7ed7f979a28d83fe37fc37ac434548cb",
+    "url": "/index.html"
+  }
+];
