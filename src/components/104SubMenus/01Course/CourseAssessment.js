fc722a6352c17071cce1c1c667fe91839f832129
melodyn99 <melodyna@joyaether.com>
2019-06-17 13:54:55 +0800

    finished submenus for all files under resource

---
+++ src/components/104SubMenus/01Course/CourseAssessment.js
@@ -0,0 +1,43 @@
+// Essential for all components
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import { withTranslation } from 'react-i18next';
+
+// Redux
+import { connect } from 'react-redux';
+
+class CourseAssessment extends Component {
+
+    render() {
+        const { //t, 
+            i18n } = this.props;
+
+        let pathname = this.props.route.location.pathname,
+            urlArray = pathname.split("/"),
+            currentPath = urlArray[2];
+
+        console.log(currentPath);
+
+        return (
+            <div className="subMenu">
+                <ul className="clearfix">
+                    <li><Link to={"/" + i18n.language + "/"}>课程资料</Link></li>
+                    <li><Link to={"/" + i18n.language + "/"}>预先准备</Link></li>
+                    <li><Link to={"/" + i18n.language + "/"}>课程教材</Link></li>
+                    <li><Link to={"/" + i18n.language + "/"}>课程作业</Link></li>
+                    <li><Link to={"/" + i18n.language + "/"}>学生管理</Link></li>
+                    <li><Link to={"/" + i18n.language + "/"}>课程公告</Link></li>
+                    <li><Link to={"/" + i18n.language + "/"}>课程问答</Link></li>
+                    <li><Link to={"/" + i18n.language + "/"}>及时记录</Link></li>
+                    <li><Link to={"/" + i18n.language + "/"} className="active">课程评分</Link></li>
+                </ul>
+            </div>
+        );
+    }
+}
+
+const mapStateToProps = (state) => ({
+    route: state.router
+});
+
+export default withTranslation()(connect(mapStateToProps)(CourseAssessment));
--- src/containers/06Resource/ResourceManagementHomework.js
+++ src/containers/06Resource/ResourceManagementHomework.js
@@ -32,6 +32,7 @@ import { getSorting } from '../../utils/02MaterialDesign/EnhancedTable';
 
 // Children components
 import BreadCrumb from '../../components/100Include/breadcrumb';
+import SubMenu from '../../components/104SubMenus/06Resource/ResourceManagementHomework';
 import EnhancedTableHead from '../../components/103MaterialDesign/EnhancedTable/EnhancedTableHead';
 import data from '../../data/06Resource/ResourceManagementHomework';
 
@@ -119,6 +120,7 @@ class ResourceManagementHomework extends React.Component {
 
                         <div className="wrapper-content">
                             <BreadCrumb />
+                            <SubMenu />
 
                             <div className="content">
                                 <Paper className={classes.paper}>
