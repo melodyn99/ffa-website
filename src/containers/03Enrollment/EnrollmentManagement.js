fb09a6778849601022ec8399701c70ff8569e57e
melodyn99 <melodyna@joyaether.com>
2019-06-17 11:36:45 +0800

    fixed naming for data files

--- src/containers/03Enrollment/EnrollmentManagement.js
+++ src/containers/03Enrollment/EnrollmentManagement.js
@@ -33,7 +33,7 @@ import { getSorting } from '../../utils/02MaterialDesign/EnhancedTable';
 // Children components
 import BreadCrumb from '../../components/100Include/breadcrumb';
 import EnhancedTableHead from '../../components/103MaterialDesign/EnhancedTable/EnhancedTableHead';
-import data from '../../data/03EnrollmentManagement/EnrollmentManagement';
+import data from '../../data/03Enrollment/EnrollmentManagement';
 
 // Define column names
 const rows = [
