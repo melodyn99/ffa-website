8ccaef68d64cbe0cfd43c250a80f74336db5e930
melodyn99 <melodyna@joyaether.com>
2019-06-18 16:13:09 +0800

    made submenu for resources management

--- src/components/104SubMenus/03SCHOOL/01Course/Course.js
+++ src/components/104SubMenus/03SCHOOL/01Course/Course.js
@@ -6,7 +6,7 @@ import { withTranslation } from 'react-i18next';
 // Redux
 import { connect } from 'react-redux';
 
-class CourseNotes extends Component {
+class Course extends Component {
 
     render() {
         const { //t, 
@@ -25,7 +25,7 @@ class CourseNotes extends Component {
                     <li><Link to={"/" + i18n.language + "/course-preparations"} className={currentPath === 'course-preparations' ? 'active' : ''}>预先准备</Link></li>
                     <li><Link to={"/" + i18n.language + "/course-materials"} className={currentPath === 'course-materials' ? 'active' : ''}>课程教材</Link></li>
                     <li><Link to={"/" + i18n.language + "/course-work"} className={currentPath === 'course-work' ? 'active' : ''}>课程作业</Link></li>
-                    <li><Link to={"/" + i18n.language + "/course-student-management"} className={currentPath === 'all-courses' ? 'active' : ''}>学生管理</Link></li>
+                    <li><Link to={"/" + i18n.language + "/course-student-management"} className={currentPath === 'course-student-management' ? 'active' : ''}>学生管理</Link></li>
                     <li><Link to={"/" + i18n.language + "/"} className={currentPath === '/' ? 'active' : ''}>课程公告</Link></li>
                     <li><Link to={"/" + i18n.language + "/course-q-and-a"} className={currentPath === 'course-q-and-a' ? 'active' : ''}>课程问答</Link></li>
                     <li><Link to={"/" + i18n.language + "/course-notes"} className={currentPath === 'course-notes' ? 'active' : ''}>及时记录</Link></li>
@@ -40,4 +40,4 @@ const mapStateToProps = (state) => ({
     route: state.router
 });
 
-export default withTranslation()(connect(mapStateToProps)(CourseNotes));
+export default withTranslation()(connect(mapStateToProps)(Course));
