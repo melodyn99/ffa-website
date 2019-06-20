40478d6fc5b962266bf860b7a1cfab10c09453bb
Henry Cheung <henrycheung@joyaether.com>
2019-06-20 18:04:32 +0800

    update

--- src/containers/01GENERAL/03Brand/SchoolAdvantage.js
+++ src/containers/01GENERAL/03Brand/SchoolAdvantage.js
@@ -2,7 +2,7 @@
 import React, { Component } from 'react';
 // import PropTypes from 'prop-types';
 // import { Redirect } from 'react-router';
-import { Link } from 'react-router-dom';
+// import { Link } from 'react-router-dom';
 import { withTranslation } from 'react-i18next';
 
 // Styling
@@ -36,9 +36,9 @@ class SchoolAdvantage extends Component {
     }
 
     render() {
-        const { i18n,
-            //classes 
-        } = this.props;
+        // const { i18n,
+        //     //classes 
+        // } = this.props;
 
         return (
             <div>
