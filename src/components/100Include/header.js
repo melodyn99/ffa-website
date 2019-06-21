1a8f577f39dd4ae56c560134cd22c2975e6a0e20
Henry Cheung <henrycheung@joyaether.com>
2019-06-21 10:52:44 +0800

    header sub-menu styling

--- src/components/100Include/header.js
+++ src/components/100Include/header.js
@@ -42,49 +42,59 @@ class Header extends Component {
                 <div className="header">
                     <div className="mobile-menu-btn">MENU</div>
 
-                    <div className="logo"></div>
+                    <h1 className="logo">
+                        <Link to={"/" + i18n.language}><img src={require('../../images/600-400.png')} alt="" /></Link>
+                    </h1>
 
                     <ul className="desktop-menu general clearfix">
-                        <li><Link to={"/" + i18n.language + "/login-with-register"} className={(currentPath === 'login-with-register') ? 'active' : ''}>入学</Link></li>
-                        <li><Link to={"/" + i18n.language + "/"} className={(currentPath === '123') ? 'active' : ''}>品牌</Link>
-                            <ul>
-                                <li><Link to={"/" + i18n.language + "/school-introduction"} className={(currentPath === 'school-introduction') ? 'active' : ''}>学院简介</Link></li>
-                                <li><Link to={"/" + i18n.language + "/school-advantage"} className={(currentPath === 'school-advantage') ? 'active' : ''}>在菲力尚学学习的优点</Link></li>
-                                <li><Link to={"/" + i18n.language + "/school-learning"} className={(currentPath === 'school-learning') ? 'active' : ''}>学生学习相关介绍</Link></li>
-                                <li><Link to={"/" + i18n.language + "/news"} className={(currentPath === 'news') ? 'active' : ''}>学院故事及新闻</Link></li>
-                                <li><Link to={"/" + i18n.language + "/activities"} className={(currentPath === 'activities') ? 'active' : ''}>学院相关活动</Link></li>
-                                <li><Link to={"/" + i18n.language + "/activities"} className={(currentPath === 'activities') ? 'active' : ''}>学院相關的企業發展和創新</Link></li>
-                                <li><Link to={"/" + i18n.language + "/school-cases"} className={(currentPath === 'school-cases') ? 'active' : ''}>和学院合作的案例</Link></li>
+                        <li className={(currentPath === 'login-with-register') ? 'active' : ''}><Link to={"/" + i18n.language + "/login-with-register"}>入学</Link>
+                            <ul className="first">
+                                <div className="arrow">need image</div>
+                                <div className="wrap">
+                                    <li className={(currentPath === 'school-introduction') ? 'active' : ''}><Link to={"/" + i18n.language + "/school-introduction"}>入學申請流程</Link></li>
+                                    <li className={(currentPath === '/') ? 'active' : ''}><Link to={"/" + i18n.language + "/"}>學科目錄</Link></li>
+                                    <li className={(currentPath === 'student-register') ? 'active' : ''}><Link to={"/" + i18n.language + "/student-register"}>報名</Link></li>
+                                </div>
                             </ul>
-
+                        </li>
+                        <li className={(currentPath === '123') ? 'active' : ''}><Link to={"/" + i18n.language + "/"}>品牌</Link>
+                            <ul className="second">
+                                <div className="arrow">need image</div>
+                                <div className="wrap">
+                                    <li className={(currentPath === 'school-introduction') ? 'active' : ''}><Link to={"/" + i18n.language + "/school-introduction"}>学院简介</Link></li>
+                                    <li className={(currentPath === 'school-advantage') ? 'active' : ''}><Link to={"/" + i18n.language + "/school-advantage"}>在菲力尚学学习的优点</Link></li>
+                                    <li className={(currentPath === 'school-learning') ? 'active' : ''}><Link to={"/" + i18n.language + "/school-learning"}>学生学习相关介绍</Link></li>
+                                    <li className={(currentPath === 'news') ? 'active' : ''}><Link to={"/" + i18n.language + "/news"}>学院故事及新闻</Link></li>
+                                    <li className={(currentPath === 'activities') ? 'active' : ''}><Link to={"/" + i18n.language + "/activities"}>学院相关活动</Link></li>
+                                    <li className={(currentPath === 'activities') ? 'active' : ''}><Link to={"/" + i18n.language + "/activities"}>学院相關的企業發展和創新</Link></li>
+                                    <li className={(currentPath === 'school-cases') ? 'active' : ''}><Link to={"/" + i18n.language + "/school-cases"}>和学院合作的案例</Link></li>
+                                </div>
+                            </ul>
                         </li>
                     </ul>
 
                     <ul className="desktop-menu student clearfix">
-                        <li><Link to={"/" + i18n.language + "/student-register-personal-information"} className={currentPath === 'student-register-personal-information' ? 'active' : ''}>报名申请</Link></li>
-                        <li><Link to={"/" + i18n.language + "/student-course"} className={currentPath === '123' ? 'active' : ''}>我的课程</Link></li>
-                        <li><Link to={"/" + i18n.language + "/"} className={currentPath === '123' ? 'active' : ''}>我的报名</Link></li>
-                        <li><Link to={"/" + i18n.language + "/"} className={currentPath === '123' ? 'active' : ''}>我的提醒</Link></li>
+                        <li className={currentPath === 'student-register-personal-information' ? 'active' : ''}><Link to={"/" + i18n.language + "/student-register-personal-information"}>报名申请</Link></li>
+                        <li className={currentPath === '123' ? 'active' : ''}><Link to={"/" + i18n.language + "/student-course"}>我的课程</Link></li>
+                        <li className={currentPath === '123' ? 'active' : ''}><Link to={"/" + i18n.language + "/"}>我的报名</Link></li>
+                        <li className={currentPath === '123' ? 'active' : ''}><Link to={"/" + i18n.language + "/"}>我的提醒</Link></li>
                     </ul>
 
                     <ul className="desktop-menu clearfix">
-                        <li><Link to={"/" + i18n.language + "/all-courses"} className={currentPath === 'all-courses' ? 'active' : ''}>课程管理</Link></li>
-                        <li><Link to={"/" + i18n.language + "/all-students-management"} className={(currentPath === 'all-students-management') || (currentPath === 'new-student') ? 'active' : ''}>学生管理</Link></li>
-                        <li><Link to={"/" + i18n.language + "/enrollment-management"} className={currentPath === 'enrollment-management' ? 'active' : ''}>报名管理</Link></li>
-                        <li><Link to={"/" + i18n.language + "/news-management"} className={(currentPath === 'news-management') || (currentPath === 'new-news') ? 'active' : ''}>新闻管理</Link></li>
-                        <li><Link to={"/" + i18n.language + "/activities-management"} className={(currentPath === 'activities-management') || (currentPath === 'new-activity') ? 'active' : ''}>活动管理</Link></li>
-                        <li><Link to={"/" + i18n.language + "/all-resources-management"}
-                            className={(currentPath === 'all-resources-management')
-                                || (currentPath === 'resources-management-course')
-                                || (currentPath === 'resources-management-homework')
-                                || (currentPath === 'new-material')
-                                || (currentPath === 'new-homework')
-                                || (currentPath === 'new-question') ? 'active' : ''}>资源管理</Link></li>
-                        <li><Link to={"/" + i18n.language + "/my-alerts"} className={currentPath === 'my-alerts' ? 'active' : ''}>我的提醒 (6)</Link></li>
-                        <li><Link to={"/" + i18n.language + "/reports"} className={currentPath === 'reports' ? 'active' : ''}>项目报告</Link></li>
-                        <li><Link to={"/" + i18n.language + "/related-courses"}
-                            className={(currentPath === 'related-courses')
-                                || currentPath === ('new-account') ? 'active' : ''}>账户管理</Link></li>
+                        <li className={currentPath === 'all-courses' ? 'active' : ''}><Link to={"/" + i18n.language + "/all-courses"}>课程管理</Link></li>
+                        <li className={(currentPath === 'all-students-management') || (currentPath === 'new-student') ? 'active' : ''}><Link to={"/" + i18n.language + "/all-students-management"}>学生管理</Link></li>
+                        <li className={currentPath === 'enrollment-management' ? 'active' : ''}><Link to={"/" + i18n.language + "/enrollment-management"}>报名管理</Link></li>
+                        <li className={(currentPath === 'news-management') || (currentPath === 'new-news') ? 'active' : ''}><Link to={"/" + i18n.language + "/news-management"}>新闻管理</Link></li>
+                        <li className={(currentPath === 'activities-management') || (currentPath === 'new-activity') ? 'active' : ''}><Link to={"/" + i18n.language + "/activities-management"}>活动管理</Link></li>
+                        <li className={(currentPath === 'all-resources-management')
+                            || (currentPath === 'resources-management-course')
+                            || (currentPath === 'resources-management-homework')
+                            || (currentPath === 'new-material')
+                            || (currentPath === 'new-homework')
+                            || (currentPath === 'new-question') ? 'active' : ''}><Link to={"/" + i18n.language + "/all-resources-management"}>资源管理</Link></li>
+                        <li className={currentPath === 'my-alerts' ? 'active' : ''}><Link to={"/" + i18n.language + "/my-alerts"}>我的提醒 (6)</Link></li>
+                        <li className={currentPath === 'reports' ? 'active' : ''}><Link to={"/" + i18n.language + "/reports"}>项目报告</Link></li>
+                        <li className={(currentPath === 'related-courses') || (currentPath === 'new-account') ? 'active' : ''}><Link to={"/" + i18n.language + "/related-courses"}>账户管理</Link></li>
                     </ul>
 
                     <div className="desktop-control">
