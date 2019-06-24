ffe9b589ae5aba3779dbc9861f7a44f4b7bd3531
melodyn99 <melodyna@joyaether.com>
2019-06-24 16:11:22 +0800

    finished renaming

---
+++ src/utils/01MaterialJsStyles/Notes/SchoolNotesTaking.js
@@ -0,0 +1,22 @@
+export const SchoolNotesTakingStyles = () => ({
+    list: {
+        padding: '10px',
+    },
+    listItem: {
+        '&:nth-of-type(even)': {
+            backgroundColor: 'rgba(0, 0, 0, 0.04)',
+        },
+        paddingTop: '20px',
+        paddingBottom: '20px',
+    },
+    listItemText: {
+        marginLeft: '10px',
+        '& p ': {
+            padding: 0,
+        },
+    },
+    typography: {
+        color: 'rgba(0, 0, 0, 0.4)',
+        fontSize: '20px',
+    },
+});
