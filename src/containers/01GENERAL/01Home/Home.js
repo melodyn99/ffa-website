f9ad131b183b15153daee5b5548026e0102dc08b
Henry Cheung <henrycheung@joyaether.com>
2019-06-25 16:46:55 +0800

    redux starting point for melody

--- src/containers/01GENERAL/01Home/Home.js
+++ src/containers/01GENERAL/01Home/Home.js
@@ -13,7 +13,6 @@ import { withTranslation } from 'react-i18next';
 
 // Api
 import { apiAuth } from '../../../Api/ApiAuth';
-import { apiConferences } from '../../../Api/ApiConferences';
 
 // Material UI
 import Grid from '@material-ui/core/Grid';
@@ -52,11 +51,11 @@ class Home extends Component {
         apiAuth.authenticate('admin@joyaether.test', 'abcd1234').then((res) => {
             // console.log(res);
             this.props.loginP(res.access_token);
-            this._getConference();
+            this._getUserInfo();
         })
     };
 
-    _getConference = () => {
+    _getUserInfo = () => {
 
         const cb = (obj) => {
             console.log("cb : ", obj);
@@ -66,7 +65,7 @@ class Home extends Component {
         }
         const params = null;
 
-        apiConferences.getConferenceFullList(params, this.props.auth.token, cb, eCb);
+        apiAuth.getUserInfo(params, this.props.auth.token, cb, eCb);
     }
 
     render() {
