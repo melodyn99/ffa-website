d6db2093fdb94dfdbc116181b667448013c5803e
Henry Cheung <henrycheung@joyaether.com>
2019-07-02 12:53:25 +0800

    integration

--- src/components/104SubMenus/03SCHOOL/01Course/SchoolAllCourse.js
+++ src/components/104SubMenus/03SCHOOL/01Course/SchoolAllCourse.js
@@ -10,22 +10,31 @@ class SchoolAllCourse extends Component {
 
     render() {
         const { //t, 
-            i18n } = this.props;
+            i18n, subject } = this.props;
+
+        console.log(subject.simpleSubject);
 
         let pathname = this.props.route.location.pathname,
             urlArray = pathname.split("/"),
             currentPath = urlArray[2];
 
-        // console.log(currentPath);
+        // console.log(oneSubject);
 
         return (
             <div className="subMenu">
                 <ul className="clearfix">
                     <li><Link to={"/" + i18n.language + "/school-all-course"} className={currentPath === 'school-all-course' ? 'active' : ''}>所有课程</Link></li>
-                    <li><Link to={"/" + i18n.language + "/school-all-course"} className={currentPath === 'course-a' ? 'active' : ''}>战略课程</Link></li>
+
+                    {(subject.simpleSubject.map((oneSubject, i) =>
+                        <li key={oneSubject.subject_id}>
+                            <Link to={"/" + i18n.language + "/school-all-course/" + oneSubject.subject_id} className={currentPath === "/school-all-course/" + oneSubject.subject_id ? 'active' : ''}>{oneSubject.name}</Link>
+                        </li>
+                    ))}
+
+                    {/* <li><Link to={"/" + i18n.language + "/school-all-course"} className={currentPath === 'course-a' ? 'active' : ''}>战略课程</Link></li>
                     <li><Link to={"/" + i18n.language + "/school-all-course"} className={currentPath === 'course-b' ? 'active' : ''}>商品管理系列课程</Link></li>
                     <li><Link to={"/" + i18n.language + "/school-all-course"} className={currentPath === 'course-c' ? 'active' : ''}>设计应用系列课程</Link></li>
-                    <li><Link to={"/" + i18n.language + "/school-all-course"} className={currentPath === 'course-d' ? 'active' : ''}>开发流程系列课程</Link></li>
+                    <li><Link to={"/" + i18n.language + "/school-all-course"} className={currentPath === 'course-d' ? 'active' : ''}>开发流程系列课程</Link></li> */}
                 </ul>
             </div>
         );
@@ -33,7 +42,8 @@ class SchoolAllCourse extends Component {
 }
 
 const mapStateToProps = (state) => ({
-    route: state.router
+    route: state.router,
+    subject: state.subjectReducer
 });
 
 export default withTranslation()(connect(mapStateToProps)(SchoolAllCourse));
