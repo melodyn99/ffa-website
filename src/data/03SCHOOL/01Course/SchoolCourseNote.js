6695d6e4127bb1a4498d535173bb5c25ee3b4767
HimFu <himfu@joyaether.com>
2019-07-08 17:17:31 +0800

    update the hardcode data of SchoolCourseNote.js

--- src/data/03SCHOOL/01Course/SchoolCourseNote.js
+++ src/data/03SCHOOL/01Course/SchoolCourseNote.js
@@ -1,20 +1,32 @@
 export default [
     {
         "id": 1,
-        "notes": "Note 1 (Click me)",
-        "file": 123,
-        "lastedit": 456,
+        "content": "Note 1 (Click me)",
+        "countsOfFile": 11,
+        "lastmoddate": 1562564980000,
     },
     {
         "id": 2,
-        "notes": "Note 2 (Click me)",
-        "file": 123,
-        "lastedit": 456,
+        "content": "Note 2 (Click me)",
+        "countsOfFile": 22,
+        "lastmoddate": 1562564980000,
     },
     {
         "id": 3,
-        "notes": "Note 3 (Click me)",
-        "file": 123,
-        "lastedit": 456,
+        "content": "Note 3 (Click me)",
+        "countsOfFile": 33,
+        "lastmoddate": 1562564980000,
+    },
+    {
+        "id": 4,
+        "content": "Note 4 (Click me)",
+        "countsOfFile": 33,
+        "lastmoddate": 1562564980000,
+    },
+    {
+        "id": 5,
+        "content": "Note 5 (Click me)",
+        "countsOfFile": 33,
+        "lastmoddate": 1562564980000,
     },
 ];
