79751dbac04eb2e24a9a42c451d099d20fffe5de
melodyn99 <melodyna@joyaether.com>
2019-07-22 11:42:21 +0800

    working on breadcrumbs

--- src/components/104SubMenus/03SCHOOL/01Course/SchoolCourse.js
+++ src/components/104SubMenus/03SCHOOL/01Course/SchoolCourse.js
@@ -23,10 +23,10 @@ class SchoolCourse extends Component {
             <div className="subMenu">
                 <ul className="clearfix">
                     <li><Link to={"/" + i18n.language + "/school-course-information"} className={currentPath === 'school-course-information' ? 'active' : ''}>课程资料</Link></li>
-                    <li><Link to={"/" + i18n.language + "/school-course-preparation"} className={currentPath === 'school-course-preparation' ? 'active' : ''}>预先准备</Link></li>
-                    <li><Link to={"/" + i18n.language + "/school-course-material"} className={currentPath === 'school-course-material' ? 'active' : ''}>课程教材</Link></li>
-                    <li><Link to={"/" + i18n.language + "/school-course-work"} className={currentPath === 'school-course-work' ? 'active' : ''}>课程作业</Link></li>
-                    <li><Link to={"/" + i18n.language + "/school-course-student-management"} className={(currentPath === 'school-course-student-management') || (currentPath === 'school-course-student-management-attendance') || (currentPath === 'school-course-student-management-homework') ? 'active' : ''}>学生管理</Link></li>
+                    <li><Link to={"/" + i18n.language + "/school-course-preparation"} className={(currentPath === 'school-course-preparation') || (currentPath === 'school-seating-plan') ? 'active' : ''}>预先准备</Link></li>
+                    <li><Link to={"/" + i18n.language + "/school-course-material"} className={(currentPath === 'school-course-material') || (currentPath === 'school-course-material-select-folder') ? 'active' : ''}>课程教材</Link></li>
+                    <li><Link to={"/" + i18n.language + "/school-course-work"} className={(currentPath === 'school-course-work') ||  (currentPath === 'school-course-work-select-folder') || (currentPath === 'school-course-work-inside-folder') || (currentPath === 'school-course-work-select-file') ? 'active' : ''}>课程作业</Link></li>
+                    <li><Link to={"/" + i18n.language + "/school-course-student-management"} className={(currentPath === 'school-course-student-management') || (currentPath === 'school-course-student-management-attendance') || (currentPath === 'school-course-student-management-homework') || (currentPath === 'school-course-student-management-select-student') ? 'active' : ''}>学生管理</Link></li>
                     <li><Link to={"/" + i18n.language + "/school-course-announcement"} className={currentPath === 'school-course-announcement' ? 'active' : ''}>课程公告</Link></li>
                     <li><Link to={"/" + i18n.language + "/school-course-q-and-a"} className={(currentPath === 'school-course-q-and-a') || (currentPath === 'school-course-reply-q-and-a') ? 'active' : ''}>课程问答</Link></li>
                     <li><Link to={"/" + i18n.language + "/school-course-note"} className={(currentPath === 'school-course-note') || (currentPath === 'school-course-new-note') || (currentPath === 'school-note-taking') ? 'active' : ''}>及时记录</Link></li>
